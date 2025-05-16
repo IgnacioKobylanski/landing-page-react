333cbb4f674a6cc71d6c649b1aca93f04985e579
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-05-16 20:46:10 -0300

    70% of the project is now ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import NuestraEsencia from './components/NuestraEsencia/NuestraEsencia'
+import Funcionalidades from './components/Funcionalidades/Funcionalidades'
+import Caracteristicas from './components/Caracteristicas/Caracteristicas'
+import OtrasFunciones from './components/OtrasFunciones/OtrasFunciones'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +15,9 @@ function App() {
       <Header />
       <Hero />
       <NuestraEsencia />
+      <Funcionalidades />
+      <Caracteristicas />
+      <OtrasFunciones />
     </>
   )
 }
---
+++ src/components/Caracteristicas/Caracteristicas.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import "../Caracteristicas/Caracteristicas.css";
+import { FaSignal, FaVectorSquare, FaUsersCog } from "react-icons/fa";
+import { TfiReload } from "react-icons/tfi";
+import { BiSolidHappyBeaming } from "react-icons/bi";
+import { MdOutlineSecurity } from "react-icons/md";
+
+const caracteristicasData = [
+  { id: 1, icon: <FaSignal size={40} />, title: "Identif. clara de los estados de producción" },
+  { id: 2, icon: <TfiReload size={40} />, title: "Trazabilidad" },
+  { id: 3, icon: <BiSolidHappyBeaming size={40} />, title: "Interfaz Amigable" },
+  { id: 4, icon: <FaVectorSquare size={40} />, title: "Interacción con otros Softwares" },
+  { id: 5, icon: <FaUsersCog size={40} />, title: "Multitasking Multiusuario" },
+  { id: 6, icon: <MdOutlineSecurity size={40} />, title: "Control de accesos y permisos" },
+];
+
+const Caracteristicas = () => {
+  return (
+    <section id="caracteristicas" className="caracteristicas">
+      <h2 className="underline-title">Características</h2>
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        centeredSlides={true}
+        breakpoints={{
+          768: {
+            slidesPerView: 1,
+          },
+          1024: {
+            slidesPerView: 1,
+          },
+        }}
+      >
+        {caracteristicasData.map(({ id, icon, title }) => (
+          <SwiperSlide key={id}>
+            <div className="caracteristica-card">
+              <div className="icon">{icon}</div>
+              <h3>{title}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Caracteristicas;
